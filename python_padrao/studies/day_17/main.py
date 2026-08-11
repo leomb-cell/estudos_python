43bb6ab792ac9369e538dab92781236027fb089c
leomb-cell <leonardomartinsbitencourte@gmail.com>
2026-08-11 13:53:44 -0300

    new entries

--- python_padrao/studies/day_17/main.py
+++ python_padrao/studies/day_17/main.py
@@ -1,6 +1,24 @@
 class User():
-    def __init__(self):
-        pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+        
+    
+
+user_1 = User("001", "Angie")
+user_2 = User("002", "Leo")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
 
 
-user_1 = User()
+print(user_2.followers)
+print(user_2.following)
