e09aac3970a51e3bc8cb19c1c713eed547d3c741
leomb-cell <leonardomartinsbitencourte@gmail.com>
2026-08-12 17:01:38 -0300

    new quiz commit

--- python_padrao/games_programs/quiz/main.py
+++ python_padrao/games_programs/quiz/main.py
@@ -1,4 +1,5 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 # creating the question bank
@@ -8,6 +9,12 @@ question_bank = []
 for question in question_data:
     question_text = question['text']
     question_answer = question['answer']
-    question_bank.append(Question(question_text, question_answer))
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
+
+newQ = QuizBrain(question_bank)
+
+while newQ.stil_has_questions():
+    newQ.next_question()
